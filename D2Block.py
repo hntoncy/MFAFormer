4ffb8bf16d2c3bc84f6d4c786a30d0556f74a2a4
Dongxu Cheng <45725441+hntoncy@users.noreply.github.com>
2026-06-26 18:09:02 +0800

    Update D2Block.py

--- D2Block.py
+++ D2Block.py
@@ -5,10 +5,10 @@ class D2Block(nn.Module):
         super().__init__()
         self.trans = Trans(in_channels , emb_size=emb_size, img_size=img_size ,num_heads = 8,dropout=0,expansion=4)
         self.CNN = fourconv(unit=4,k=7,in_channels=in_channels,out_channels=out_channels,win_size=11, size_average=True, sigma=1.5)
-    def forward(self, x,y,z):#x是MAC的输出(跳跃连接），y是上一层CE（解码器），z是MSS的输出
+    def forward(self, x,y,z):#x是CFE的输出(跳跃连接），y是上一层D2（解码器），z是MLFS的输出
         a = self.CNN(y,z)
         a = torch.sigmoid(a)
-        b = a+x #2017有改动/3d用的*
+        b = a+x 
         b = self.trans(b)
         b = torch.sigmoid(b)
         # print("D2_b=",b.shape)
